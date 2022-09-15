bf2ce7c200cb8da318bbe6b3ee2cc82e48182726
Lari <larissaborsari@hotmail.com>
2022-09-15 14:26:34 -0300

    last commit

--- inout.py
+++ inout.py
@@ -1,2 +1,9 @@
 #input sempre entra como string
 
+
+
+nome = input("Informe seu nome: ")
+sobrenome = input("Informe seu sobrenome: ")
+idade = input("Informe sua idade: ")
+
+print("{} {} tem {} anos de idade!" .format(nome, sobrenome, idade))
