7242775d7f83b1663d42550c67d50f2baef6584f
Lari <larissaborsari@hotmail.com>
2022-09-15 13:43:20 -0300

    last commit

--- first.py
+++ first.py
@@ -1,1 +1,11 @@
-print('hello world!')
+#tipos de dados
+
+print("-" * 20, end=' ')
+print('hello world!', end=' ')
+print("-" * 20)
+print("-" * 25, end=' ')
+print(1 + 10, end=' ')
+print("-" * 25)
+print("-" * 24, end=' ')
+print(1.5 + 5 + 0.63, end=' ')
+print("-" * 24)
