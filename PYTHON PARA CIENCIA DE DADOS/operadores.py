24d899ccd9be12d36c70437c53de6ad0e3b215f8
Lari <larissaborsari@hotmail.com>
2022-09-15 14:53:44 -0300

    last commit

---
+++ PYTHON PARA CIENCIA DE DADOS/operadores.py
@@ -0,0 +1,44 @@
+#ARITMETICOS
+
+produto_1 = 10
+produto_2 = 20
+
+print("adição ", produto_1 + produto_2)
+print("subtração ", produto_1 - produto_2)
+print("multiplicação ", produto_1 * produto_2)
+print("divisão ", produto_1 / produto_2)
+print("inteiro ", produto_1 // produto_2)
+print("módulo ", produto_1 % produto_2)
+print("potência ", produto_1 ** produto_2)
+
+#COMPARAÇÃO
+
+saldo = 200
+saque = 200 
+
+print(saldo == saque)
+print(saldo != saque)
+print(saldo > saque)
+print(saldo >= saque)
+print(saldo <= saque)
+print(saldo < saque)
+
+#ATRIBUIÇÃO
+
+saldo_2 = 500
+print(saldo_2)
+
+saldo_2 += 200
+print(saldo_2)
+
+saldo_2 -= 200
+print(saldo_2)
+
+saldo_2 *= 2
+print(saldo_2)
+
+saldo_2 /= 2
+print(saldo_2)
+
+saldo_2 **= 2
+print(saldo_2)
